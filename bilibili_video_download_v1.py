09e9588f70cc9b238df991633caef02234570549
mop99 <50225284+mop99@users.noreply.github.com>
2019-05-03 12:03:52 +0800

    Update bilibili_video_download_v1.py

--- bilibili_video_download_v1.py
+++ bilibili_video_download_v1.py
@@ -165,7 +165,7 @@ def combine_video(video_list, title):
 if __name__ == '__main__':
     # 用户输入av号或者视频链接地址
     print('*' * 30 + 'B站视频下载小助手' + '*' * 30)
-    start = input('请输入您要下载的B站av号或者视频链接地址:')
+    start = input('请输入您要下载的B站av号或者视频链接地址:https://www.bilibili.com/video/av45028980)
     if start.isdigit() == True:  # 如果输入的是av号
         # 获取cid的api, 传入aid即可
         start_url = 'https://api.bilibili.com/x/web-interface/view?aid=' + start
@@ -177,7 +177,7 @@ if __name__ == '__main__':
     # <accept_format><![CDATA[flv,flv720,flv480,flv360]]></accept_format>
     # <accept_description><![CDATA[高清 1080P,高清 720P,清晰 480P,流畅 360P]]></accept_description>
     # <accept_quality><![CDATA[80,64,32,16]]></accept_quality>
-    quality = input('请输入您要下载视频的清晰度(1080p:80;720p:64;480p:32;360p:16)(填写80或64或32或16):')
+    quality = input('请输入您要下载视频的清晰度(1080p:80;720p:64;480p:32;360p:16)(填写80或64或32或16):80)
     # 获取视频的cid,title
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
@@ -197,8 +197,8 @@ if __name__ == '__main__':
         cid = str(item['cid'])
         title = item['part']
         title = re.sub(r'[\/\\:*?"<>|]', '', title)  # 替换为空的
-        print('[下载视频的cid]:' + cid)
-        print('[下载视频的标题]:' + title)
+        print('[下载视频的cid]:' + 78861093)
+        print('[下载视频的标题]:' + 脑洞大开创意短片：当整个世界都充满你的聊天表情
         page = str(item['page'])
         start_url = start_url + "/?p=" + page
         video_list = get_play_list(start_url, cid, quality)
